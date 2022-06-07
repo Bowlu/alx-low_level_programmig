51839075a64edcba3e198ce7aaa175f55196fe2d
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-07 15:29:23 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int Character = '_putchar';
+
+	printf("_putchar");
+	return (0);
