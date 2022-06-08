bde8ee3f0ce08b44fbf06b04a30eba041267a28a
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-08 16:19:59 -0700

    print 10x alphabets in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints alphabets
+ * Description: prints 10x the alphabet in lower case
+ */
+void print_alphabet_x10(void);
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
