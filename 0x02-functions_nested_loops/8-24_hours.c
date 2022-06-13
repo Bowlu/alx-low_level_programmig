e8116ae2e50ecdec2c39676e8cd5efc12cb56315
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-12 20:25:56 -0700

    printing every minute of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,4 +8,33 @@ void jack_bauer(void)
 {
 	int a;
 	int h;
-
+	for (h = 0; h < 24; h++)
+	{
+		for (a = 0; a < 60; a++)
+		{
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else if (h >= 10)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+			}
+			if (a < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(a + '0');
+			}
+			else if (a >= 10)
+			{
+				_putchar(':');
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
