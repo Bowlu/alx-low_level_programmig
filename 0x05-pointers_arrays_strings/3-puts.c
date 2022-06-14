1618b0ce07316c4d1b38bbb7e7cee3765dce4b15
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-14 11:49:53 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function name
+ * @str: parameter
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	char string[] = "C programming is simple";
+str = string;
+}
