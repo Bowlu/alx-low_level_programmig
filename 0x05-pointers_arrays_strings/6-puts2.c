5fb9441e2935e2c99925589bff36cd65a420fea6
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-14 12:57:47 -0700

    printing characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - function name
+ * @str: parameter
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int bet = str
+
+	for (i = 0; i < bet; i++)
+	{
+		putchar(str);
+	}
+	putchar('\n');
+}
