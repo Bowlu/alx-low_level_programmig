0f29bbb4330e3e60f90167059c0951a7862b9f46
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-14 11:26:36 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - function name
+ * @s: character parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char len[] = "My first strlen";
+
+	s = len;
+	putchar(*s);
+	putchar('\n');
+}
