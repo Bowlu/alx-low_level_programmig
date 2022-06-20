daa1015a8780ba3f42612643a3f74b3546624f42
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-20 01:41:18 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,15 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int a, b;
 
-	for (i = 0; i < 10; i++)
+	for (a = 0; a < 10; a++)
 	{
-		for (j = 0; j < 15; j++)
+		for (b = 0; b < 15; b++)
 		{
-			_putchar(j + '0');
+			_putchar((b / 10) + '0');
 		}
+		_putchar((a % 10) + '0');
 		_putchar('\n');
 	}
 }
