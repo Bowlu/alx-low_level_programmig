d8e2c3c84a16efe8aeca549629412e4daad2104a
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-06-21 22:43:25 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,23 +14,23 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d\n", n);
 		}
-			else
-			{
-				printf("%d, ", n);
+		else
+		{
+			printf("%d, ", n);
 			n = n + 1;
-			}
+		}
 	}
-			else if (n > 98)
-			{
-				while (n >= 98)
-				{
-					if (n == 98)
-					printf("%d\n", n);
-				}
-				else
-				{
-					printf("%d, ", n);
-				n = n - 1;
-				}
-			}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+			n = n - 1;
+		}
+	}
 }
