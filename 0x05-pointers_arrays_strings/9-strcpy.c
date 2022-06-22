df5f62f5606dab90dcc5146738d903f495b31342
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-22 15:18:51 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcpy - pointer
+ * @dest: first pointer
+ * @src: second pointer
+ * Return: value of the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		src[a] = dest[a];
+	}
+	dest[a++] = '\0';
+	return (dest);
+}
