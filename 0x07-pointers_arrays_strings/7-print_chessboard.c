9501d7eb94f82e7b9fbb1e0d9851dcefa898ba77
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-06-23 22:43:18 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - function name
@@ -15,7 +16,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			putchar(a[i][j]);
 		}
 		putchar('\n');
 	}
