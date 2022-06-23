a6cc7e9593c64a864562c09aa787ed6413664917
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-06-23 19:59:21 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - function name
@@ -11,7 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		putchar('\n');
 		return;
 	}
 	_putchar(*s);
