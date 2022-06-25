cdd9a9129a1d66577af832204a306d96c91ae8de
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-25 04:24:46 -0700

    c-argc, argv files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the function that prints its name
+ * @argc: argument count
+ * @argv: array of argument
+ * 
+ * Return: 0
+ */
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
