3db1c02e223b1bbcfb074a388c383834b8f2cac7
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-25 05:42:57 -0700

    sum of positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the sum ob positive numbers
+ * @argc: argument count
+ * @argv: an array of the argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int num, addNum;
+
+	addNum += num;
+
+	if (argc == 1)
+		printf("%d\n", "0");
+
+	else if (argv[num] > '9' || argv[num] < '0')
+	{
+		printf("Error\n");
+		return (1);
+	}
