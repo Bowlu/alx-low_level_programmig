8510feaa7a41d4b416fbf69f53469cae892d6ea6
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-25 04:51:51 -0700

    multiplication of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the multiplication of two numbers
+ * @argc: argument count
+ * @argv: an array of the argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+	int num1, num2;
+
+	mul = num1 * num2;
+	if (argc != 3)
+		printf("Error\n");
+	return (1);
+
+	else
+		printf("%d\n", mul);
+	return (0);
+}
