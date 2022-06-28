eddd03ea634aece83e387c5d12a0f338ff9fe478
Awoyemi <awoyemiboluwatife@gmail.com>
2022-06-28 04:46:52 -0700

    a pointer to a new string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function that returns a pointer to a new string duplicated
+ * @str: string 
+ * Return: NULL if str is NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *n;
+	int i;
+
+	for (i = 0; i > 0; i++)
+	{
+		n = malloc(i * sizeo(char));
+		*n = *str;
+	}
+	free(n);
+
+	if (str == NULL)
+	       return (NULL);
+	else
+	return (n);
+}	
