96e9525e4512c5d6c789d34475aa5fd4ad33b6ed
Awoyemi <awoyemiboluwatife@gmail.com>
2022-07-06 13:31:06 -0700

    defining a new structure type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+// structure declaration for the first task
+
+/**
+ * struct dog - structure with a variable name 'dog'
+ * @name: first member of the structure
+ * @age: second member
+ * @owner: third member
+ *
+ * Return: SUCCESS
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* STRUCT_DOG */
+
