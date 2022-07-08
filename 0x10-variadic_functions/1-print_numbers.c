7bdb27725a32ae4fe2d04ca999614d4eca6dc615
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-07-08 06:08:53 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,21 +6,21 @@
  * @n: number of integers passed to the function
 
  * Return: 0
-/
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list list;
-unsigned int a;
-va_start(lis, n);
-for (a = 0; a < n; a++)
-{
-printf("%dd", va_arg(list, int));
-if (separator && a < n - 1)
-printf("%s", separator);
-}
-printf("\n");
-va_end(list);
+  va_list list;
+  unsigned int a;
+  va_start(lis, n);
+  for (a = 0; a < n; a++)
+  {
+    printf("%dd", va_arg(list, int));
+    if (separator && a < n - 1)
+      printf("%s", separator);
+  }
+  printf("\n");
+  va_end(list);
 }
 
 
