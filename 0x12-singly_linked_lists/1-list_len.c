f50635ffbc704de7c4e2fc93200ee2ca52fa0bb6
Awoyemi <awoyemiboluwatife@gmail.com>
2022-07-19 11:54:37 -0700

    singly linked tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns then number of elements in a linked list
+ * @h: singly linked list
+ * Return: number of elements in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t element;
+
+	element = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		element++;
+	}
+	return (element);
+}
